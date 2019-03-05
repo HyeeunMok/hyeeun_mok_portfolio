6220cec911e0d107037861728761d2965948541d
Hyeeun <hyeeuni.mok@gmail.com>
2019-03-04 23:57:24 -0500

    menu tabs

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ class Index extends React.Component {
 
     render() {
         return (
-            <h1>I am Index Page from Class Component</h1>
+            <div>
+                <h1>I am Index Page from Class Component</h1>
+                <a href="/">HOME</a>
+                <a href="/about">ABOUT</a>
+                <a href="/projects">PROJECTS</a>
+                <a href="/blogs">BLOG</a>
+                <a href="/resume">Resume</a> 
+            </div>
         )
     }
 }
