a2ab2915fc7eb43a415d67f71de3a956e16ea4bc
Hyeeun <hyeeuni.mok@gmail.com>
2019-03-06 07:19:53 -0500

    starting BaseLayout

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Header from '../components/shared/Header';
+import BaseLayout from '../components/layouts/BaseLayout';
 
 class Index extends React.Component {
 
     render() {
         return (
-            <div>
+            <BaseLayout>
                 <h1>I am Index Page from Class Component</h1>
-                <Header title ={'I am a header component'}>
-                    <h1>I am header subtitle</h1>
-                </Header>
-            </div>
+            </BaseLayout>
         )
     }
 }
 
 export default Index;
+
+
+{/* <Header title ={'I am a header component'}>
+<h1>I am header subtitle</h1>
+</Header> */}
