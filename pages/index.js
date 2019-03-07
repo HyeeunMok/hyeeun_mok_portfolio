00da0688fdb0c6c5847a346875350a0d27edce4f
Hyeeun <hyeeuni.mok@gmail.com>
2019-03-07 07:32:07 -0500

    created supercomponent

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,43 @@ import BaseLayout from '../components/layouts/BaseLayout';
 
 class Index extends React.Component {
 
+    constructor() {
+        super();
+        
+        this.state = {
+            title: 'I am Index Page'
+        }
+
+        console.log('constructor');
+    }
+
+    componentDidMount() {
+        console.log('constructor');
+    }
+
+    componentDidUpdate() {
+        console.log('constructor');
+    }
+
+    componentWillUnmount() {
+        console.log('constructor');
+    }
+
+    updateTitle() {
+        this.setState({title: 'I am Updated Index page'});
+    }
+
     render() {
+        console.log('constructor');
+
         return (
             <BaseLayout>
-                <h1>I am Index Page from Class Component</h1>
+                <h1 className='fromPage'>I am Index Page from Class Component</h1>
+                <h2> { this.state.title} </h2>
+                <button onClick={ () => this.updateTitle() }> Change Title </button>
             </BaseLayout>
         )
     }
 }
 
 export default Index;
-
-
-{/* <Header title ={'I am a header component'}>
-<h1>I am header subtitle</h1>
-</Header> */}
