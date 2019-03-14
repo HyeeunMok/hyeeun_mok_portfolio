8f81ec8c48e38290da6def2ead8f220efb923fe6
Hyeeun <hyeeuni.mok@gmail.com>
2019-03-13 23:35:50 -0400

    creating project page

---
+++ pages/project.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+
+class Project extends React.Component {
+
+    render() {
+        return (
+            <BaseLayout>
+                <h1>I am a Project Page </h1>
+            </BaseLayout>
+        )
+    }
+}
+
+export default Project;
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
+import Link from 'next/link';
+
 import axios from 'axios';
 
 class Projects extends React.Component {
