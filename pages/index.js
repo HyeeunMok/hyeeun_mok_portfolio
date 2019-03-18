17bffd5592d82527076473b9d206233961b51121
Hyeeun <hyeeuni.mok@gmail.com>
2019-03-17 23:46:46 -0400

    add reactstrap

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,16 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-import axios from 'axios';
 
-import SuperComponent from '../components/SuperComponent';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Index extends SuperComponent {
-
-    static async getInitialProps() {
-        let userData = {};
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-            userData = response.data;
-        } catch(err) {
-            console.error(err);
-        }
-        
-
-        return {initialData: [1,2,3,4], userData};
-    }
-
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            title: 'I am Index Page'
-        }
-
-        console.log('constructor');
-    }
-
-    componentDidMount() {
-        console.log('componentDidMount');
-    }
-
-    componentDidUpdate() {
-        console.log('componentDidUpdate');
-    }
-
-    componentWillUnmount() {
-        console.log('componentWillUnmount');
-    }
-
-    updateTitle = () => {
-        this.setState({title: 'I am Updated Index page'});
-    }
+class Index extends React.Component {
 
     render() {
-        const { title } = this.state;
-        const { userData, initialData } = this.props;
-        // const title = this.state.title;
-
+       
         return (
             <BaseLayout>
-                <h1 className='fromPage'>I am Index Page from Class Component</h1>
-                <h2> { title } </h2>
-                <h2> { userData.title } </h2>
-                <button onClick={this.updateTitle}> Change Title </button>
+                <Button color="danger">Danger!</Button>
             </BaseLayout>
         )
     }
