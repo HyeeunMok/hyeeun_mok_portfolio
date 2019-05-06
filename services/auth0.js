db5749f31279ef8e0bf9d08f9c13d2757fa4ddca
Hyeeun <hyeeuni.mok@gmail.com>
2019-05-06 08:28:50 -0400

    save token in cookie

--- services/auth0.js
+++ services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth0 {
 
@@ -12,12 +13,13 @@ constructor() {
     });
     
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
 
 }
 
 handleAuthentication() {
-    debugger;
     return new Promise((resolve, reject) => {
         this.auth0.parseHash((err, authResult) => {
             if (authResult && authResult.accessToken && authResult.idToken) {
@@ -26,19 +28,46 @@ handleAuthentication() {
             } else if (err) {
                 reject(err);
                 console.log(err);
-                alert(`Error: ${err.error}. Check the console for further details.`);
+                // alert(`Error: ${err.error}. Check the console for further details.`);
             }
         });
     }) 
 }
 
-setSession() {
-    // Save tokens!!!!
+setSession(authResult) {
+    debugger;
+  // Set the time that the Access Token will expire at
+    const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+
+    // this.accessToken = authResult.accessToken;
+
+    Cookies.set('user', authResult.idTokenPayload);
+    Cookies.set('jwt', authResult.idToken);
+    Cookies.set('expiresAt', expiresAt);
 }
 
-  login() {
-    this.auth0.authorize();
-  }
+logout(){
+    
+    Cookies.remove('user');
+    Cookies.remove('jwt');
+    Cookies.remove('expiresAt');
+
+    this.auth0.logout({
+        returnTo: '',
+        clientID: 'B3EPbm6jQQzDFIN0nBklXCKxm82UkEhs'
+    })
+}
+
+login() {
+   this.auth0.authorize();
+ }
+
+ isAuthenticated() {
+   // Check whether the current time is past the
+   // access token's expiry time
+   const expiresAt = Cookies.getJSON('expiresAt');
+   return new Date().getTime() < expiresAt;
+ }
 }
 
 const auth0Client = new Auth0();
